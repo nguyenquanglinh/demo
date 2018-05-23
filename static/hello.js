b7764cc0060de5421ca6a250bfcb2be1b0d757e5
quang <thangnguyen2000x99nd@gmail.com>
2018-05-23 21:43:51 +0700

    Initial commit

---
+++ static/hello.js
@@ -0,0 +1,67 @@
+$( document ).ready(function() {
+
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    var strTime ="time: "+ hours + ':' + minutes +'h '+ 'Date: '+day+':'+month+':'+year
+    return strTime;
+}
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time){
+    if (time === undefined){
+        time = 0;
+    }
+    var control = "";
+    var date = formatAMPM(new Date());
+    control = '<li style="width:100%">' +
+                        '<div class="msj macro">' +
+                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ who.avatar +'" /></div>' +
+                            '<div class="text text-l">' +
+                                '<p>'+ text +'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</li>';
+
+    setTimeout(
+        function(){
+            $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
+        }, time);
+
+}
+
+function resetChat(){
+    $("ul").empty();
+}
+
+$(".mytext").keydown( function(e){
+
+    if (e.which == 13){
+        var text = $(this).val();
+        if (text !== ""){
+            insertChat("me", text);
+            $(this).val('');
+        }
+    }
+});
+
+
+$('body > div > div > div:nth-child(2) > span').click(function(){
+    $(".mytext").trigger({type: 'keydown', which: 13, keyCode: 13});
+})
+
+//-- Clear Chat
+resetChat();
+
+//-- Print Messages
+});
+
+
+
+
+//-- NOTE: No use time on insertChat.
